5c6a5ca6890d2c8e775c48c17d451e285061d72a
aabeborn <aabeborn@benato.dev>
2024-01-30 23:26:37 +0100

    feat(prettier): create prettier shareable config

---
+++ prettier.js
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+export default {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "none",
+  singleAttributePerLine: true,
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: true,
+  proseWrap: "never",
+  insertPragma: false,
+  printWidth: 80,
+  requirePragma: false,
+  tabWidth: 4,
+  useTabs: true,
+  embeddedLanguageFormatting: "auto",
+};
